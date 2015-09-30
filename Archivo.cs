ca3e007219839884c4a13bbdb0c9d32e24f2ee9f
Adrián <adrian.ipod25@gmail.com>
2015-09-30 18:10:07 -0500

    Clase Archivo

---
+++ Archivo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexico
+{
+    class Archivo
+    {
+        private string path = AppDomain.CurrentDomain.BaseDirectory.ToString() + "Archivos\\";
+        /// <summary>
+        //Constructor de la clase Archivo 
+        /// </summary>
+        public Archivo()
+        {
+            
+        }
+        
+    }
+}
