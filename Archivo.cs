f4e68944c4ae04b12915cf77cd7a38032616e362
Adrián <adrian.ipod25@gmail.com>
2015-10-01 22:45:03 -0500

    Metodos para agregar texto al archivo de errores y tabla de simbolos, verificar que existe un archivo y crear uno nuevo

--- Archivo.cs
+++ Archivo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,55 @@ namespace AnalizadorLexico
     class Archivo
     {
         private string path = AppDomain.CurrentDomain.BaseDirectory.ToString() + "Archivos\\";
+        private string tablaSimbolos = String.Empty;
+        private string archivoErrores = String.Empty;
+
         /// <summary>
         //Constructor de la clase Archivo 
         /// </summary>
-        public Archivo()
+        public Archivo(string tablaSimbolos,string archivoErrores)
         {
-            
+            this.archivoErrores = archivoErrores;
+            this.tablaSimbolos = tablaSimbolos;
         }
-        
+
+        /// <summary>
+        //Metodo que verifica la existencia de un archivo 
+        /// </summary>
+        public bool existeArchivo(string nombre)
+        {
+            return (File.Exists(path + nombre)) ? true:false;
+        }
+
+        /// <summary>
+        //Metodo que crea el Archivo se sugiere preguntar con el metodo
+        //existeArchivo() si ya existe primero 
+        /// </summary>
+        public void creaArchivo(string nombre)
+        {
+
+            StreamWriter sw = File.CreateText(path+nombre);
+            sw.Close();
+        }
+
+        /// <summary>
+        //Metodo que añade texto a la tabla de Simbolos
+        /// </summary>
+        public void appendTextToTabla(string s)
+        {
+            StreamWriter sw = File.AppendText(path+tablaSimbolos);
+            sw.WriteLine(s);
+            sw.Close();
+        }
+
+        /// <summary>
+        //Metodo que añade texto al Archivo de Errores
+        /// </summary>
+        public void appendTextToErrors(string s)
+        {
+            StreamWriter sw = File.AppendText(path + archivoErrores);
+            sw.WriteLine(c);
+            sw.Close();
+        }
     }
 }
