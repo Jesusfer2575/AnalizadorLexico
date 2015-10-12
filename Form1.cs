c358b118d92c89a115169888ce9b62ce385916b3
Adrián Fernández <adrian.ipod25@gmail.com>
2015-10-12 17:55:23 -0500

    Cambios a la manera de leer caracteres del .c

--- Form1.cs
+++ Form1.cs
@@ -14,14 +14,70 @@ namespace AnalizadorLexico
 {
     public partial class Form1 : Form
     {
+        private string componente_Acumulado = String.Empty;
+        private int ini = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Metodo que verifica que un caracter sea letra o guion bajo
+        /// </summary>
+        private bool esLetra(char c)
+        {
+            return (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')) || (c == '_')) ? true : false;
+        }
+
+        /// <summary>
+        /// Metodo que verifica que un caracter sea numero
+        /// </summary>
+        private bool esNumero(char c)
+        {
+            return ((c >= 0) && (c <= 9)) ? true : false;
+        }
+
+        private void escribe(string componente,int identificador)
+        {
+            switch (identificador)
+            {
+                case 4:
+
+            }
+        }
+
+        private void Clasifica(string linea)
+        {
+            ComponenteLexico cl = new ComponenteLexico();
+            int tam = linea.Length;
+            for (int i = ini; i < tam; i++)
+            {
+                char c = linea[i];
+                if (esLetra(c))
+                {
+                    int j;
+                    for (j = i;esLetra(c) && cl.automataIdentificador(c);j++)
+                    {
+                        componente_Acumulado += c;
+                    }
+                    i = j;
+                    escribe(componente_Acumulado,identificador);
+                    componente_Acumulado = String.Empty;
+                }
+                else if (esNumero(c))
+                {
+
+                }
+                else
+                {
+                    //Aqui agregar preguntas si es +,-,/,*,etc.
+                }
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int indice_automata = 0;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 StreamReader sr = new StreamReader(openFileDialog1.FileName);
@@ -29,37 +85,15 @@ namespace AnalizadorLexico
                 string tabla = textBoxTabla.Text;
                 string errores = textBoxErrores.Text;
                 string posibleSimbolo = String.Empty;
-                ComponenteLexico cl = new ComponenteLexico();
-
+                
                 Archivo ar = new Archivo(tabla, errores);
                 ar.creaArchivoTabla();
                 ar.creaArchivoErrores();
-
-                bool flag1 = true;
+                
                 while (!sr.EndOfStream){
                     string  linea = sr.ReadLine();
                     int tam = linea.Length;
-                    for(int i=0;i<tam;i++)
-                    {
-                        flag1 = (cl.automataIdentificador(linea[i]) == true) ? true : false;
-                        /*if (indice_automata == 0)
-                        {
-                            
-                            if (!flag1)
-                                indice_automata += 1;
-                        }
-                        else if (indice_automata == 1) {
-                            flag1 = (cl.automataEntero(linea[i]) == true) ? true : false;
-                            if (!flag1)
-                                indice_automata += 1;
-                        }*/
-                        
-                    }
-
-                    if (flag1)
-                        Console.WriteLine("Cadena Aceptada!");
-                    else
-                        Console.WriteLine("Cadena Rechazada");
+                    Clasifica(linea);
                 }
                 
                 sr.Close();
