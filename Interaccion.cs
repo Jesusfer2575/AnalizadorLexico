8f82e56e1520bb2d82c41719ae60012efcbc1fc0
Adrián <adrian.ipod25@gmail.com>
2015-10-12 17:54:31 -0500

    Clase interaccion

---
+++ Interaccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexico
+{
+    class Interaccion
+    {
+        public static int SUMA = 1;
+        public static int SUMA_ASIGNACION = 2;
+        public static int SUMA_INCREMENTO = 3;
+        public static int IDENTIFICADOR=4;
+    }
+}
